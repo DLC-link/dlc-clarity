b242583e176a55d0b51c0eed6f556e00e5da9bd3
scolear <bobtera@gmail.com>
2023-08-16 15:15:53 +0200

    chore: hide more stuff

--- scripts/network-configs.ts
+++ scripts/network-configs.ts
@@ -16,14 +16,14 @@ const mocknet = {
 };
 
 const mocknet_cloud = {
-  network: new StacksMocknet({ url: 'https://dev-oracle.dlc.link' }),
+  network: new StacksMocknet({ url: process.env.MOCKNET_ADDRESS as string }),
   contractAddress: 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM',
   exampleContractAddress: 'STNHKEPYEPJ8ET55ZZ0M5A34J0R3N5FM2CMMMAZ6',
   privateKey: '753b7cc01a1a2e86221266a154af739463fce51219d97e4f856cd7200c3bd2a601',
   protocolPrivateKey: 'de433bdfa14ec43aa1098d5be594c8ffb20a31485ff9de2923b2689471c401b801',
   mnemonic:
     'twice kind fence tip hidden tilt action fragile skin nothing glory cousin green tomorrow spring wrist shed math olympic multiply hip blue scout claw',
-  api_base: 'https://dev-oracle.dlc.link',
+  api_base: process.env.MOCKNET_ADDRESS as string,
 };
 
 const testnet = {
@@ -33,7 +33,7 @@ const testnet = {
   privateKey: process.env.PRIVATE_KEY as string,
   protocolPrivateKey: process.env.PROTOCOL_PRIVATE_KEY as string,
   mnemonic: process.env.MNEMONIC as string,
-  api_base: 'https://stacks-node-api.testnet.stacks.co',
+  api_base: 'https://api.testnet.hiro.so',
 };
 
 const mainnet = {
@@ -43,7 +43,7 @@ const mainnet = {
   privateKey: process.env.PRIVATE_KEY as string,
   protocolPrivateKey: process.env.PROTOCOL_PRIVATE_KEY as string,
   mnemonic: process.env.MNEMONIC as string,
-  api_base: 'https://stacks-node-api.stacks.co',
+  api_base: 'https://api.hiro.so',
 };
 
 const environments = {
