9bc3b4e073a322b9914d200f7dc013c3ff83cce2
scolear <bobtera@gmail.com>
2023-08-24 00:58:02 +0200

    update scripts

--- scripts/06_sample-setup-loan.ts
+++ scripts/06_sample-setup-loan.ts
@@ -1,24 +1,12 @@
 import { AnchorMode, SignedContractCallOptions, bufferCV, uintCV } from '@stacks/transactions';
 import { network, exampleContractAddress, exampleContractName, sendContractCall, testCreatorKey } from './common';
 
-export default async function setupLoan(
-  btcDeposit: number,
-  liqRatio: number,
-  liqFee: number,
-  ERT: number,
-  attestorIDs: number[]
-) {
+export default async function setupLoan(btcDeposit: number, attestorIDs: number[]) {
   const txOptions: SignedContractCallOptions = {
     contractAddress: exampleContractAddress,
     contractName: exampleContractName,
     functionName: 'setup-loan',
-    functionArgs: [
-      uintCV(btcDeposit),
-      uintCV(liqRatio),
-      uintCV(liqFee),
-      uintCV(ERT),
-      bufferCV(Buffer.from(attestorIDs)),
-    ],
+    functionArgs: [uintCV(btcDeposit), bufferCV(Buffer.from(attestorIDs))],
     senderKey: testCreatorKey,
     validateWithAbi: true,
     network,
